c82784a1fd110e07c669df8a01d1f0c80d12df74
DHEERAJ2AUS <108796274+DHEERAJ2AUS@users.noreply.github.com>
2023-03-22 21:04:10 +0530

    new

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/firebase'
+import 'firebase/storage'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB6fz_-Kf75rUEBgNSlkoYhCej3X55wLWo",
+    authDomain: "cloudside-e9ca7.firebaseapp.com",
+    projectId: "cloudside-e9ca7",
+    storageBucket: "cloudside-e9ca7.appspot.com",
+    messagingSenderId: "743516632086",
+    appId: "1:743516632086:web:71cfe689d81bdb207af137"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
